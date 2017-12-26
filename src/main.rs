8ea2e6ab92a0d67110e99412ff0377280d6a41fa
Brandur <brandur@brandur.org>
2017-12-26 01:06:57 -0700

    Very nominal progress

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,46 @@ use juniper_iron::{GraphQLHandler, GraphiQLHandler};
 use mount::Mount;
 use time::precise_time_ns;
 
-struct Database;
+struct Root;
 
-impl Database {
-    fn new() -> Database {
-        Database {}
+impl Root {
+    fn new() -> Root {
+        Root {}
     }
 }
 
-impl Context for Database {}
+impl Context for Root {}
 
-graphql_object!(Database: Database as "Query" |&self| {
+graphql_object!(Root: Root as "Root" |&self| {
     description: "The root query object of the schema"
+
+    field foo() -> String {
+        "Bar".to_owned()
+    }
 });
+
+#[derive(GraphQLObject)]
+#[graphql(description="Information about a person")]
+struct Person {
+    #[graphql(description="The person's full name, including both first and last names")]
+    name: String,
+
+    #[graphql(description="The person's age in years, rounded down")]
+    age: i32,
+}
+
+#[derive(GraphQLObject)]
+struct House {
+    address: Option<String>, // Converted into String (nullable)
+    inhabitants: Vec<Person>, // Converted into [Person!]!
+}
 
 struct ResponseTime;
 
 impl typemap::Key for ResponseTime { type Value = u64; }
 
-fn context_factory(_: &mut Request) -> Database {
-    Database::new()
+fn context_factory(_: &mut Request) -> Root {
+    Root::new()
 }
 
 impl BeforeMiddleware for ResponseTime {
@@ -54,8 +74,8 @@ impl AfterMiddleware for ResponseTime {
 fn main() {
     let graphql_endpoint = GraphQLHandler::new(
         context_factory,
-        Database::new(),
-        EmptyMutation::<Database>::new(),
+        Root::new(),
+        EmptyMutation::<Root>::new(),
     );
     let graphiql_endpoint = GraphiQLHandler::new("/graphql");
 
