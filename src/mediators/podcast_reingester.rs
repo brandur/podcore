0b4525a91f498dd3e8005254c5ac80a6dddd7bb9
Brandur <brandur@brandur.org>
2018-01-09 09:33:44 -0800

    Add debug information

--- src/mediators/podcast_reingester.rs
+++ src/mediators/podcast_reingester.rs
@@ -4,6 +4,8 @@ use mediators::podcast_updater::PodcastUpdater;
 use schema::{podcast_feed_contents, podcast_feed_locations, podcasts};
 use url_fetcher::URLFetcherPassThrough;
 
+use diesel;
+use diesel::pg::Pg;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use r2d2::Pool;
@@ -58,21 +60,24 @@ impl PodcastReingester {
         let res = common::log_timed(&log.new(o!("step" => "query_podcasts")), |ref _log| {
             // Note that although in SQL a subselect can be coerced into a single value, Diesel's
             // type system cannot support this. We workaround by storing these values to Vec<_>.
-            podcasts::table
-                .select((
-                    podcasts::id,
-                    podcast_feed_contents::table
-                        .filter(podcast_feed_contents::podcast_id.eq(podcasts::id))
-                        .order(podcast_feed_contents::retrieved_at.desc())
-                        .limit(1)
-                        .select(podcast_feed_contents::content),
-                    podcast_feed_locations::table
-                        .filter(podcast_feed_locations::podcast_id.eq(podcasts::id))
-                        .order(podcast_feed_locations::last_retrieved_at.desc())
-                        .limit(1)
-                        .select(podcast_feed_locations::feed_url),
-                ))
-                .load::<(i64, Vec<String>, Vec<String>)>(conn)
+            let query = podcasts::table.select((
+                podcasts::id,
+                (podcast_feed_contents::table
+                    .filter(podcast_feed_contents::podcast_id.eq(podcasts::id))
+                    .order(podcast_feed_contents::retrieved_at.desc())
+                    .limit(1)
+                    .select(podcast_feed_contents::content)),
+                (podcast_feed_locations::table
+                    .filter(podcast_feed_locations::podcast_id.eq(podcasts::id))
+                    .order(podcast_feed_locations::last_retrieved_at.desc())
+                    .limit(1)
+                    .select(podcast_feed_locations::feed_url)),
+            ));
+
+            let debug = diesel::debug_query::<Pg, _>(&query).to_string();
+            info!(log, "Debug query"; "query" => debug);
+
+            query.load::<(i64, Vec<String>, Vec<String>)>(conn)
         })?;
 
         for &(ref _podcast_id, ref content_vec, ref feed_url_vec) in &res {
