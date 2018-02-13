c8900ee2eddce43ea7cada922cb9575c4e613771
Brandur <brandur@brandur.org>
2018-02-13 11:38:29 -0800

    Drop PodcastFeedContentCompressor

--- src/mediators/mod.rs
+++ src/mediators/mod.rs
@@ -4,6 +4,5 @@ pub mod directory_podcast_searcher;
 pub mod directory_podcast_updater;
 pub mod error_reporter;
 pub mod podcast_crawler;
-pub mod podcast_feed_content_compressor;
 pub mod podcast_reingester;
 pub mod podcast_updater;
