e9f15c96092227c47e68d8a6a9f1098e62fc5a8a
Brandur <brandur@brandur.org>
2018-04-03 08:53:25 -0700

    Move to new `resource` module convention for all GraphQL objects

--- src/graphql/operations.rs
+++ src/graphql/operations.rs
@@ -3,7 +3,6 @@ use mediators;
 use model;
 use schema;
 
-use chrono::{DateTime, Utc};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use juniper;
@@ -83,104 +82,8 @@ graphql_object!(
 );
 
 //
-// GraphQL resources
-//
-
-mod resource {
-    use model;
-
-    #[derive(GraphQLObject)]
-    pub struct AccountPodcast {
-        #[graphql(description = "The account podcast's ID.")]
-        pub id: String,
-    }
-
-    impl<'a> From<&'a model::AccountPodcast> for AccountPodcast {
-        fn from(e: &model::AccountPodcast) -> Self {
-            AccountPodcast {
-                id: e.id.to_string(),
-            }
-        }
-    }
-}
-
-//
 // Queries
 //
-
-#[derive(GraphQLObject)]
-struct EpisodeObject {
-    #[graphql(description = "The episode's ID.")]
-    pub id: String,
-
-    #[graphql(description = "The episode's description.")]
-    pub description: Option<String>,
-
-    #[graphql(description = "Whether the episode is considered explicit.")]
-    pub explicit: Option<bool>,
-
-    #[graphql(description = "The episode's web link.")]
-    pub link_url: Option<String>,
-
-    #[graphql(description = "The episode's media link (i.e. where the audio can be found).")]
-    pub media_url: String,
-
-    #[graphql(description = "The episode's podcast's ID.")]
-    pub podcast_id: String,
-
-    #[graphql(description = "The episode's publishing date and time.")]
-    pub published_at: DateTime<Utc>,
-
-    #[graphql(description = "The episode's title.")]
-    pub title: String,
-}
-
-impl<'a> From<&'a model::Episode> for EpisodeObject {
-    fn from(e: &model::Episode) -> Self {
-        EpisodeObject {
-            id:           e.id.to_string(),
-            description:  e.description.clone(),
-            explicit:     e.explicit,
-            link_url:     e.link_url.clone(),
-            media_url:    e.media_url.to_owned(),
-            podcast_id:   e.podcast_id.to_string(),
-            published_at: e.published_at,
-            title:        e.title.to_owned(),
-        }
-    }
-}
-
-#[derive(GraphQLObject)]
-struct PodcastObject {
-    // IDs are exposed as strings because JS cannot store a fully 64-bit integer. This should be
-    // okay because clients should be treating them as opaque tokens anyway.
-    #[graphql(description = "The podcast's ID.")]
-    pub id: String,
-
-    #[graphql(description = "The podcast's image URL.")]
-    pub image_url: Option<String>,
-
-    #[graphql(description = "The podcast's language.")]
-    pub language: Option<String>,
-
-    #[graphql(description = "The podcast's RSS link URL.")]
-    pub link_url: Option<String>,
-
-    #[graphql(description = "The podcast's title.")]
-    pub title: String,
-}
-
-impl<'a> From<&'a model::Podcast> for PodcastObject {
-    fn from(p: &model::Podcast) -> Self {
-        PodcastObject {
-            id:        p.id.to_string(),
-            image_url: p.image_url.clone(),
-            language:  p.language.clone(),
-            link_url:  p.link_url.clone(),
-            title:     p.title.to_owned(),
-        }
-    }
-}
 
 #[derive(Default)]
 pub struct Query;
@@ -195,7 +98,7 @@ graphql_object!(Query: Context |&self| {
     }
 
     field episode(&executor, podcast_id: String as "The podcast's ID.") ->
-            FieldResult<Vec<EpisodeObject>> as "A collection episodes for a podcast." {
+            FieldResult<Vec<resource::Episode>> as "A collection episodes for a podcast." {
         let id = i64::from_str(podcast_id.as_str()).
             chain_err(|| "Error parsing podcast ID")?;
 
@@ -207,12 +110,12 @@ graphql_object!(Query: Context |&self| {
             .load::<model::Episode>(&*context.conn)
             .chain_err(|| "Error loading episodes from the database")?
             .iter()
-            .map(EpisodeObject::from)
+            .map(resource::Episode::from)
             .collect::<Vec<_>>();
         Ok(results)
     }
 
-    field podcast(&executor) -> FieldResult<Vec<PodcastObject>> as "A collection of podcasts." {
+    field podcast(&executor) -> FieldResult<Vec<resource::Podcast>> as "A collection of podcasts." {
         let context = executor.context();
         let results = schema::podcast::table
             .order(schema::podcast::title.asc())
@@ -220,8 +123,106 @@ graphql_object!(Query: Context |&self| {
             .load::<model::Podcast>(&*context.conn)
             .chain_err(|| "Error loading podcasts from the database")?
             .iter()
-            .map(PodcastObject::from)
+            .map(resource::Podcast::from)
             .collect::<Vec<_>>();
         Ok(results)
     }
 });
+
+//
+// GraphQL resources
+//
+
+mod resource {
+    use model;
+
+    use chrono::{DateTime, Utc};
+
+    #[derive(GraphQLObject)]
+    pub struct AccountPodcast {
+        #[graphql(description = "The account podcast's ID.")]
+        pub id: String,
+    }
+
+    impl<'a> From<&'a model::AccountPodcast> for AccountPodcast {
+        fn from(e: &model::AccountPodcast) -> Self {
+            AccountPodcast {
+                id: e.id.to_string(),
+            }
+        }
+    }
+
+    #[derive(GraphQLObject)]
+    pub struct Episode {
+        #[graphql(description = "The episode's ID.")]
+        pub id: String,
+
+        #[graphql(description = "The episode's description.")]
+        pub description: Option<String>,
+
+        #[graphql(description = "Whether the episode is considered explicit.")]
+        pub explicit: Option<bool>,
+
+        #[graphql(description = "The episode's web link.")]
+        pub link_url: Option<String>,
+
+        #[graphql(description = "The episode's media link (i.e. where the audio can be found).")]
+        pub media_url: String,
+
+        #[graphql(description = "The episode's podcast's ID.")]
+        pub podcast_id: String,
+
+        #[graphql(description = "The episode's publishing date and time.")]
+        pub published_at: DateTime<Utc>,
+
+        #[graphql(description = "The episode's title.")]
+        pub title: String,
+    }
+
+    impl<'a> From<&'a model::Episode> for Episode {
+        fn from(e: &model::Episode) -> Self {
+            Episode {
+                id:           e.id.to_string(),
+                description:  e.description.clone(),
+                explicit:     e.explicit,
+                link_url:     e.link_url.clone(),
+                media_url:    e.media_url.to_owned(),
+                podcast_id:   e.podcast_id.to_string(),
+                published_at: e.published_at,
+                title:        e.title.to_owned(),
+            }
+        }
+    }
+
+    #[derive(GraphQLObject)]
+    pub struct Podcast {
+        // IDs are exposed as strings because JS cannot store a fully 64-bit integer. This should
+        // be okay because clients should be treating them as opaque tokens anyway.
+        #[graphql(description = "The podcast's ID.")]
+        pub id: String,
+
+        #[graphql(description = "The podcast's image URL.")]
+        pub image_url: Option<String>,
+
+        #[graphql(description = "The podcast's language.")]
+        pub language: Option<String>,
+
+        #[graphql(description = "The podcast's RSS link URL.")]
+        pub link_url: Option<String>,
+
+        #[graphql(description = "The podcast's title.")]
+        pub title: String,
+    }
+
+    impl<'a> From<&'a model::Podcast> for Podcast {
+        fn from(p: &model::Podcast) -> Self {
+            Podcast {
+                id:        p.id.to_string(),
+                image_url: p.image_url.clone(),
+                language:  p.language.clone(),
+                link_url:  p.link_url.clone(),
+                title:     p.title.to_owned(),
+            }
+        }
+    }
+}
