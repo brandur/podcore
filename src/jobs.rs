71e031d2ccc641bbaf45d279ed801ef477999d50
Brandur <brandur@brandur.org>
2018-05-06 14:28:39 -0700

    Basic tests for existing jobs

--- src/jobs.rs
+++ src/jobs.rs
@@ -3,7 +3,15 @@ pub mod no_op {
 
     use slog::Logger;
 
+    //
+    // Public constants
+    //
+
     pub const NAME: &str = "no_op";
+
+    //
+    // Public types
+    //
 
     #[derive(Clone, Debug, Deserialize, Serialize)]
     pub struct Args {
@@ -20,14 +28,43 @@ pub mod no_op {
             Ok(())
         }
     }
+
+    //
+    // Tests
+    //
+
+    #[cfg(test)]
+    mod tests {
+        use jobs::no_op::*;
+        use test_helpers;
+
+        #[test]
+        fn test_job_no_op_run() {
+            Job {
+                args: Args {
+                    message: "Hello, world".to_owned(),
+                },
+            }.run(&test_helpers::log())
+                .unwrap();
+        }
+    }
 }
+
 pub mod verification_mailer {
     use errors::*;
     use http_requester::HttpRequester;
 
     use slog::Logger;
 
+    //
+    // Public constants
+    //
+
     pub const NAME: &str = "verification_mailer";
+
+    //
+    // Public types
+    //
 
     #[derive(Clone, Debug, Deserialize, Serialize)]
     pub struct Args {
@@ -43,6 +80,33 @@ pub mod verification_mailer {
     impl<'a> Job<'a> {
         pub fn run(&self, _log: &Logger) -> Result<()> {
             Ok(())
+        }
+    }
+
+    //
+    // Tests
+    //
+
+    #[cfg(test)]
+    mod tests {
+        use http_requester::HttpRequesterPassThrough;
+        use jobs::verification_mailer::*;
+        use test_helpers;
+
+        use std::sync::Arc;
+
+        #[test]
+        fn test_job_verification_mailer_run() {
+            Job {
+                args:      Args {
+                    to:    "foo@example.com".to_owned(),
+                    token: "verification-token".to_owned(),
+                },
+                requester: &HttpRequesterPassThrough {
+                    data: Arc::new(Vec::new()),
+                },
+            }.run(&test_helpers::log())
+                .unwrap();
         }
     }
 }
